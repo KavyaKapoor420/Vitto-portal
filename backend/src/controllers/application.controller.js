55d01fe01624db80a887f114e173bde2a026314d
kavyakapoor420 <kavyakapoor412@gmail.com>
2026-05-30 23:16:11 +0530

    done seperate MVC logic

--- backend/src/controllers/application.controller.js
+++ backend/src/controllers/application.controller.js
@@ -38,5 +38,69 @@ const getApplication=async(req,res)=>{
     }
 }
 
+const postApplication=async(req,res)=>{
 
-module.exports={getApplication}
+
+    const errors=validateApplication(req.body)
+
+
+    if(errors.length>0){
+        return res.status(400).json({error:"Validation failed ",details:error})
+    }
+
+    const {name,mobile,amount,purpose,language}=req.body ;
+
+
+    try{
+         const application=await prisma.application.create({
+            data:{
+                name:name.trim(),
+                mobile:mobile.trim() ,
+                amount:Number(amount),
+                purpose:purpose.trim(),
+                language:language
+            },
+            select:{
+                id:true,
+                name:true,
+                createdAt:true 
+            }
+         })
+
+         res.status(201).json({ message: "Application submitted successfully", application })
+    }catch(err){
+        res.status(500).json({ error: "An error occurred while submitting the application" })
+    }
+}
+
+
+const updateApplicationStatus=async(req,res)=>{
+     const {id}=req.params 
+
+     const {status}=req.body 
+
+     const validStatus=['approved','rejected']
+
+     if(!status || !validStatus.includes(status)){
+        return res.status(400).json({error:`status must be 'approved' or 'rejected' `})
+     }
+
+     try{
+         const application=await prisma.application.update({
+            where:{id:id},
+            data:{status:status}
+         })
+         res.json(application)
+
+
+     }catch(err){
+
+        if(err.code==='P2025'){
+            return res.status
+        }
+        console.error(err)
+        res.status(500).json({error:"An error occurred while updating the application"})
+     }
+}
+
+module.exports={getApplication,postApplication,updateApplicationStatus}
--- backend/src/routes/application.route.js
+++ backend/src/routes/application.route.js
@@ -3,44 +3,11 @@ const express=require('express')
 
 const applicationRouter=express.Router() 
 const prisma=require('../prisma')
-const { getApplication } = require('../controllers/application.controller')
+const { getApplication,postApplication,updateApplicationStatus } = require('../controllers/application.controller')
 
 // post /api/applications 
 
-applicationRouter.post('/',async(req,res)=>{
-
-
-    const errors=validateApplication(req.body)
-
-
-    if(errors.length>0){
-        return res.status(400).json({error:"Validation failed ",details:error})
-    }
-
-    const {name,mobile,amount,purpose,language}=req.body ;
-
-
-    try{
-         const application=await prisma.application.create({
-            data:{
-                name:name.trim(),
-                mobile:mobile.trim() ,
-                amount:Number(amount),
-                purpose:purpose.trim(),
-                language:language
-            },
-            select:{
-                id:true,
-                name:true,
-                createdAt:true 
-            }
-         })
-
-         res.status(201).json({ message: "Application submitted successfully", application })
-    }catch(err){
-        res.status(500).json({ error: "An error occurred while submitting the application" })
-    }
-})
+applicationRouter.post('/',postApplication)
 
 
 // get /api/applications 
@@ -48,34 +15,7 @@ applicationRouter.get("/",getApplication)
 
 //patch /api/applications/:id
 
-applicationRouter.patch("/:id",async(req,res)=>{
-     const {id}=req.params 
-
-     const {status}=req.body 
-
-     const validStatus=['approved','rejected']
-
-     if(!status || !validStatus.includes(status)){
-        return res.status(400).json({error:`status must be 'approved' or 'rejected' `})
-     }
-
-     try{
-         const application=await prisma.application.update({
-            where:{id:id},
-            data:{status:status}
-         })
-         res.json(application)
-
-
-     }catch(err){
-
-        if(err.code==='P2025'){
-            return res.status
-        }
-        console.error(err)
-        res.status(500).json({error:"An error occurred while updating the application"})
-     }
-})
+applicationRouter.patch("/:id",updateApplicationStatus)
 
 
 module.exports=applicationRouter
